0b02bf8ce7729a39a3b51b8e4603b066a027d25a
Caio Bukvar <caiobukvar@gmail.com>
2023-04-21 10:40:05 -0300

    subindo pro git

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -2,28 +2,7 @@ import style from './style.module.scss'
 
 function Socials() {
   return (
-    <div className={style.user__socials}>
-      <div>
-        <img src="" alt="location" />
-        <p>Location</p>
-      </div>
-
-      <div>
-        <img src="" alt="github" />
-        <p>github</p>
-      </div>
-
-      <div>
-        <img src="" alt="twitter" />
-        <p>Twitter</p>
-      </div>
-
-      <div>
-        <img src="" alt="current-company" />
-        <p>@companyname</p>
-      </div>
-
-    </div>
+    
   );
 }
 
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,12 @@
-import Socials from '../../components/Socials';
 import style from './style.module.scss'
+import LocationIcon from '../../assets/icon-location.svg';
+import GitHubIcon from '../../assets/icon-website.svg';
+import TwitterIcon from '../../assets/icon-twitter.svg';
+import CompanyIcon from '../../assets/icon-company.svg';
+import AvatarDefault from '../../assets/avatar-default.svg';
+import LupaIcon from '../../assets/lupa.svg';
+import Moon from '../../assets/icon-moon.svg';
+import Sun from '../../assets/icon-sun.svg';
 
 function Main({ colorTheme, setColorTheme }) {
 
@@ -18,20 +25,22 @@ function Main({ colorTheme, setColorTheme }) {
           <h1>devfinder</h1>
           <button onClick={() => changeColorTheme()}>
             {colorTheme === 'dark' ? 'LIGHT' : 'DARK'}
+            <img src={colorTheme === 'dark' ? Sun : Moon} alt="theme" />
           </button>
         </div>
 
         <div className={style.search__bar}>
+          <img src={LupaIcon} alt="lupa" />
           <input type="text" placeholder='Search GitHub username...' />
           <button className={style.search__button}>Search</button>
         </div>
 
         <div className={style.main__content}>
           <div className={style.user__info}>
-            <img src="" alt="profile-pic" />
+            <img src={AvatarDefault} alt="profile-pic" />
             <div>
               <h3>User Name</h3>
-              <p>user @username </p>
+              <p className={style.username}>user @username </p>
               <p>Join date</p>
             </div>
           </div>
@@ -47,21 +56,42 @@ function Main({ colorTheme, setColorTheme }) {
           <div className={style.user__git__info}>
             <div>
               <p>Repos</p>
-              <p>Num</p>
+              <b>Num</b>
             </div>
 
             <div>
               <p>Followers</p>
-              <p>Num</p>
+              <b>Num</b>
             </div>
 
             <div>
               <p>Following</p>
-              <p>Num</p>
+              <b>Num</b>
             </div>
           </div>
 
-          <Socials />
+          <div className={style.user__socials}>
+            <div>
+              <img src={LocationIcon} alt="location" />
+              <p>Location</p>
+            </div>
+
+            <div>
+              <img src={GitHubIcon} alt="github" />
+              <p>github</p>
+            </div>
+
+            <div>
+              <img src={TwitterIcon} alt="twitter" />
+              <p>Twitter</p>
+            </div>
+
+            <div>
+              <img src={CompanyIcon} alt="current-company" />
+              <p>@companyname</p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
